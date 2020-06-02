b6699069f68c46755860a773a4eda8e01b7b6917
Burcak Yesilirmak <52253791+BurcakYesilirmak@users.noreply.github.com>
2020-06-02 14:20:18 +0300

    Create test3.c

---
+++ test3.c
@@ -0,0 +1,251 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include<math.h>
+#include<float.h>
+#include <time.h>
+#include <limits.h>
+#define C 134217729 
+
+void VeltkampSplit(double x, double *xh, double *xl) {        //unsigned long C = B^(t/2) + 1;  //s=27 & B=2;
+double p = C * x;
+double q = x - p;
+*xh = p + q; 
+*xl = x - *xh;
+}
+
+void F2Sum(double a, double b,double *s, double *t) {
+double z;
+*s= a+b;
+z = *s - a;
+*t= b-z;
+return ;
+}
+void TwoSum(double a, double b,double *s, double *t) {
+double z ,a1 ,b1 ,del_a ,del_b;
+*s= a+b;
+a1= *s-b;
+b1= *s-a1;
+del_a = a-a1;
+del_b = b-b1;
+*t = del_a+del_b;
+return ;
+}
+
+//PRIST doubly compensated summation algorithm
+void Summation(double xh1, double xl1, double xh2, double xl2,double *xh3, double *xl3 ){   
+  double t,s,u,v,z;
+  F2Sum(xh1, xh2, &t, &s);
+  F2Sum(xl1, xl2, &u, &v);
+  z = s+v+u;
+  F2Sum(t, z,xh3,xl3);
+  /* *xh3=t;
+     *xl3=z; */
+return ;
+}
+
+//Pichat and Neumaier’s summation algorithm
+void Summation2(double xh1, double xl1, double xh2, double xl2,double *xh3, double *xl3 ){  
+  double t,s,u,v,z;
+  TwoSum(xh1, xh2, &t, &s);
+  TwoSum(xl1, xl2, &u, &v);
+  z= s+v+u; 
+  //F2Sum(t, z, xh3, xl3);
+  *xh3=t;
+  *xl3=z; 
+ return ;
+}   
+
+// Dekker's & Polynomial Multiplication
+void Mult(double xh1, double xl1, double xh2, double xl2,double *xh3, double *xl3) {
+double xh_1, xl_1, xh_2, xl_2, t, z;
+VeltkampSplit(xh1, &xh_1, &xl_1); 
+VeltkampSplit(xh2, &xh_2, &xl_2); 
+*xh3 =  xh1*xh2;
+*xl3 =  xh_1*xh_2 - *xh3;
+*xl3 +=  xh_1*xl_2;
+*xl3 +=  xl_1*xh_2;
+*xl3 +=  xl_1*xl_2+ xl1*xh2 + xh1*xl2;
+return;
+}
+
+void Division(double xh1, double xl1, double xh2, double xl2,double *xh3, double *xl3){
+double u1, u0, t, z;
+t =xh1/xh2;
+Mult(t, 0, xh2, 0,&u0,&u1); 
+z = xh1- u0;
+z -= u1;
+z += xl1;
+z -= *xh3 * xl2;
+z /= xh2;  
+F2Sum(t, z, xh3, xl3);
+return;
+} 
+
+   void Inverse_SquareRoot(double x1[3], double x2[3], double x3[3], double x4[3],double *xh3, double *xl3) {  //as pairs
+      double xh, xl, x_h, x_l, y0, y1, y2, y3, d0, d1, R3_0, R3_1;
+      double u0, u1, uu0, uu1, v0, v1, p0, p1, t0, t1 ,t2, t3, t4, t5, t6, t7;
+
+      Mult(x1[0],x2[0],x3[0],x4[0],&u0,&u1); // starting to scalar product 
+      Mult(x1[1],x2[1],x3[1],x4[1],&v0,&v1); 
+      Mult(x1[2],x2[2],x3[2],x4[2],&t0,&t1);  
+      Summation2(u0,u1,v0,v1,&t2,&t3);
+      Summation2(t0,t1,t2,t3,&t4,&t5);
+
+     y0 = sqrt(t4);                          //starting to the square root 
+     VeltkampSplit(y0,&xh, &xl) ;
+     Mult(xh, xl, xh, xl,&p0,&p1); 
+     y1 = t4 - p0;
+     y1 -= p1;
+     y1 += t5 ;
+     if(y0<0) { 
+     printf("error \n"); }
+     y1 = (y1 / y0)*0.5;         // y0 and y1 are pairs of the squareroot operation
+
+     Mult(y0, y1, y0, y1, &uu0,&uu1); 
+     Mult(y0, y1, uu0, uu1, &R3_0,&R3_1);    //pairs of R^3
+
+    // divide with 1's pairs with R^3 's pairs
+     y2=1;
+     *xh3 = y2/R3_0;
+     VeltkampSplit(*xh3, &xh, &xl) ;
+     VeltkampSplit(y2, &x_h, &x_l) ;
+     Mult(xh, xl, x_h, x_l,&t6,&t7); 
+     *xl3 = y2 - t6;
+     *xl3 -= t7;
+     *xl3 += y3;
+     *xl3 -= *xh3 * R3_1;
+     *xl3 /= R3_0;      
+     }
+
+
+
+   void unit_vector(double X, double Y, double Z, double *X_unit, double *Y_unit, double *Z_unit) {
+        double alpha_old, alpha_new, F, dF, eps ,f, scalar_new, max;
+        int count,i;
+    //finding maximum absolute value of the vector
+    double k[]={ X, Y, Z }; 
+    max=fabs(k[1]);
+    for(i=0;i<3;i++) {
+    if(fabs(k[i])>max) 
+    max=fabs(k[i]);
+    }
+    printf("Original Vector :\n");
+    printf("x[0]=%.16le\n",X);
+    printf("x[1]=%.16le\n",Y);
+    printf("x[2]=%.16le\n",Z);
+
+    //initilial value of alpha
+    alpha_old = 1/max;
+    do {
+        count ++ ;
+       alpha_new = alpha_old ;
+       scalar_new =alpha_new*sqrt(X*X+Y*Y+Z*Z) ;
+       F = scalar_new-1 ;
+       dF =  sqrt(X*X+Y*Y+Z*Z) ;
+       alpha_old = alpha_new - F/dF ;  
+       printf("count=%d alpha=%.16le f=%.16le\n",count,alpha_old,alpha_old*sqrt(X*X+Y*Y+Z*Z)-1);    
+           }
+    while (alpha_old*sqrt(X*X+Y*Y+Z*Z) != 1);
+    printf("Unit Vector :\n");
+    printf("x[0]=%.16le\n",X*alpha_new);
+    printf("x[1]=%.16le\n",Y*alpha_new);
+    printf("x[2]=%.16le\n",Z*alpha_new);
+    *X_unit = X*alpha_new;
+    *Y_unit = Y*alpha_new;
+    *Z_unit = Z*alpha_new;
+    }   
+
+
+int main(int argc, char **argv)
+{
+   int i;
+//double xh1=0.37797,xl1=0.000293,xh2=0.335593,xl2=0.0001233, xh3=0.3355674, I, J, K;
+double xh1=-1.0,xh2=1.0,xh3=1.0, I, J, K;
+double x_pair1[] ={0.3,0.5,0.7};         // P1={ah,bh,ch}
+double x_pair2[] ={0.003,0.005,0.007};   // P2={al,bl,cl}
+double c, c3, sum, sum_h, sum_l, t_h, t_l, prod, prod_h, prod_l, ch, cl;
+double xh6, xl6, xh, xl;
+double relative_errorr[i],relative_error_calc[i];
+long double summ, cc, prod_long, c_long ;
+
+    FILE *kk;
+    kk=fopen("SUM.txt","w");
+    //usual double
+    sum=0.0;
+    c=3.0;
+
+    //usual long double 
+    summ=0.0;
+    cc=3.0;
+
+    // with algoritm
+    sum_h=0.0;
+    sum_l=0.0;
+    ch =3.0;
+    cl = 0.0;
+    xh6=3.0;
+    xl6=0.0;
+
+    for (i=0; i<35; i++){
+          //ORDINARY DOUBLE SUMMATION
+    c=c/3.0;
+    sum=sum+c;
+         //ORDINARY LONG-DOUBLE SUMMATION
+    cc=cc/3.0;
+    summ=summ+cc;
+         //SUMMATION WITH ARITHMETIC OPERATION
+    Division(ch, cl, xh6, xl6, &ch, &cl);
+    Summation2(sum_h, sum_l, ch, cl, &sum_h, &sum_l);
+    fprintf(kk,"%d %.20Le %.20Le %.20Le\n",i, summ, -(sum-summ)/summ, -(sum_h-summ+sum_l)/summ);  
+    } 
+
+    FILE *kkk,*fff;
+    kkk=fopen("MULT.txt","w");
+    //fff=fopen("MULT2.txt","w");
+    prod=1.0;
+    c3= 1/100000.0+1.0;
+
+    prod_long=1.0;
+    c_long= 1/100000.0+1.0;
+  
+    prod_h=1.0;
+    prod_l=0.0;    
+    t_h=1.0;
+    t_l=0.0; 
+    Division(1.0, 0.0, 100000.0, 0.0, &xh, &xl); // c3= 1/100000.0+1.0;
+    printf("xh= %.16le xl=%.16le\n",xh,xl);
+    Summation2(xh, xl, 1, 0, &ch, &cl);    
+    printf("ch= %.16le cl=%.16le\n",ch,cl);
+    printf("c3=%.16le\n",c3);
+    printf("c_long=%.16Le\n",c_long); 
+
+    double *relative_error_ord = (double*)malloc(500);
+    if (relative_error_ord != NULL) {  
+
+    for(i=0; i<100000; i++) { 
+    // ORDINARY DOUBLE MULTIPLICATION
+    prod= prod*c3;
+
+    // ORDINARY LONG DOUBLE MULTIPLICATION
+    prod_long = prod_long*c_long;
+
+    // MULTIPLICATION WITH ARITHMETIC OPERATION        
+    Mult(prod_h, prod_l, c3, 0, &prod_h, &prod_l);
+    Mult(t_h, t_l, ch, cl, &t_h, &t_l);
+    fprintf(kkk,"%d %.22Le %.22Le %.22Le  \n",i, prod_long, -(prod-prod_long)/prod_long, -(prod_h-prod_long+prod_l)/prod_long);
+    //fprintf(fff,"%d %.22Le %.22Le %.22Le  \n",i, prod_long, -(prod-prod_long)/prod_long, -(t_h-prod_long+t_l)/prod_long);
+    } 
+            printf("prod_h= %.16le prod_l=%.16le\n",prod_h,prod_l);
+            printf("th= %.16le tl=%.16le\n",t_h,t_l);
+
+fclose(kk);
+fclose(kkk); 
+//fclose(fff); 
+unit_vector(xh1, xh2, xh3, &I, &J, &K);
+return (0); 
+} 
+}
+
+
+
