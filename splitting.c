3fae7e75bb76cf1a86c607a7d5609ea842f710bb
Burcak Yesilirmak <52253791+BurcakYesilirmak@users.noreply.github.com>
2020-07-22 21:39:12 +0300

    Update splitting.c

--- splitting.c
+++ splitting.c
@@ -2,51 +2,6 @@
   #include <stdio.h>
   #include <math.h>
   #include <string.h>
-/* void split(char *buffer, double *xh, double *xl){
-     FILE *fp; 
-     char ch[24], res[30], rest[10] , rest2[10], ress[20];
-     char *ptr;
-     double number1, number4;
-     long int number2, number3, x1;
-     int m, i, k, xx, digit_num_after_dec, zeros;
-     fp = fopen("myfile.txt", "r");
-     if(!fp)
-    {
-        printf("Could not open file\n");
-    } 
-        fgets(buffer, 50, fp);
-        sscanf(buffer, "%s" , ch);
-        sscanf(buffer, "%lf" , &number1); //original number taken from the buffer
-        sprintf(res,"%.23lf", number1);  // assign number1 to a string
-
-     xx = sizeof(ch) ;   //length of the array
-     gcvt(number1,23,ress);	// convert number1 to a string array
-     // compare original and converted strings and find the starting different point 
-	printf("buffer converted is: %s\n",ress);
-    printf("buffer2 original is: %s\n",ch);
-     for(m=0; m<23; m++) {                 //find the similar finish point (m)
-             if(ch[m]!=ress[m])
-                           break;
-     }                                // m=17 
-          printf("point :\n%d\n", m); //different point with integer and comma 
-   // convert different character parts to long integer
-                for(int i=m, k=0; i<=xx && k<xx-m ;i++, k++) {  
-     rest[k]= ch[i];
-     rest2[k]= ress[i];}     
-     // converts both strings to the long integer 
-    sscanf(rest, "%ld" , &number2);
-    sscanf(rest2, "%ld" , &number3);  
-          printf("residual string for converted number:\n%ld\n", number3);  
-          printf("residual string for original number:\n%ld\n", number2); 
-          x1=(number3-number2);                   //find subtraction of long integers -- should it be labs?
-          printf("difference:\n%ld\n", x1); 
-          number4=x1*pow(10,-xx+2); // 2-> take into account integer and comma 
-        printf("xl:\n%le\n", number4);
-        printf("xh:\n%.25lf\n", number1); 
-        *xh=number1;
-        *xl=number4;
-             fclose(fp);
-}  */
   int main(int argc, char* argv[])
 {
     char buffer[50];
@@ -105,7 +60,52 @@ FILE *fp;
              fclose(fp);
      return (0);
 }
-   
+   // another solution
+/* void split(char *buffer, double *xh, double *xl){
+     FILE *fp; 
+     char ch[24], res[30], rest[10] , rest2[10], ress[20];
+     char *ptr;
+     double number1, number4;
+     long int number2, number3, x1;
+     int m, i, k, xx, digit_num_after_dec, zeros;
+     fp = fopen("myfile.txt", "r");
+     if(!fp)
+    {
+        printf("Could not open file\n");
+    } 
+        fgets(buffer, 50, fp);
+        sscanf(buffer, "%s" , ch);
+        sscanf(buffer, "%lf" , &number1); //original number taken from the buffer
+        sprintf(res,"%.23lf", number1);  // assign number1 to a string
+
+     xx = sizeof(ch) ;   //length of the array
+     gcvt(number1,23,ress);	// convert number1 to a string array
+     // compare original and converted strings and find the starting different point 
+	printf("buffer converted is: %s\n",ress);
+    printf("buffer2 original is: %s\n",ch);
+     for(m=0; m<23; m++) {                 //find the similar finish point (m)
+             if(ch[m]!=ress[m])
+                           break;
+     }                                // m=17 
+          printf("point :\n%d\n", m); //different point with integer and comma 
+   // convert different character parts to long integer
+                for(int i=m, k=0; i<=xx && k<xx-m ;i++, k++) {  
+     rest[k]= ch[i];
+     rest2[k]= ress[i];}     
+     // converts both strings to the long integer 
+    sscanf(rest, "%ld" , &number2);
+    sscanf(rest2, "%ld" , &number3);  
+          printf("residual string for converted number:\n%ld\n", number3);  
+          printf("residual string for original number:\n%ld\n", number2); 
+          x1=(number3-number2);                   //find subtraction of long integers -- should it be labs?
+          printf("difference:\n%ld\n", x1); 
+          number4=x1*pow(10,-xx+2); // 2-> take into account integer and comma 
+        printf("xl:\n%le\n", number4);
+        printf("xh:\n%.25lf\n", number1); 
+        *xh=number1;
+        *xl=number4;
+             fclose(fp);
+}  */
 
 
 
