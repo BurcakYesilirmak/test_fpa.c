04aacdba61d2c361f3ef5e8745057330f47b574c
Burcak Yesilirmak <52253791+BurcakYesilirmak@users.noreply.github.com>
2020-09-23 23:30:03 +0300

    Update fpa_arithmetic_operations.c

--- fpa_arithmetic_operations.c
+++ fpa_arithmetic_operations.c
@@ -1,3 +1,6 @@
+// This code is a tool which constructs the floating point arithmetic operations
+// Including accuracy test for each operation
+// Burcak Yesilirmak
 
 #include<stdlib.h>
 #include<stdio.h>
@@ -79,23 +82,41 @@ return;
     xl3 =  xh_1*xh_2 - xh3;
     xl3 +=  xh_1*xl_2;
     xl3 +=  xl_1*xh_2;
-    xl3 +=  xl_1*xl_2+ xl1*xh2 + xh1*xl2;
+    xl3 +=  xl_1*xl_2+ xl1*xh2 + xh1*xl2; // test it
+   // xl3 +=  xl_1*xl_2; // finish like this in the Müller's book
     F2Sum(xh3, xl3, t, z);
     return;
-    }
+    } 
+
+    // Dekker's Two Product
+    void Mult2(double xh1, double xl1, double xh2, double xl2,double *t, double *z) {
+    double p, q, r;
+    double xh_1, xl_1, xh_2, xl_2, xh3, xl3;
+    VeltkampSplit(xh1, &xh_1, &xl_1); 
+    VeltkampSplit(xh2, &xh_2, &xl_2); 
+    p=xh_1*xh_2;
+    q=xh_1*xl_2+xl_1*xh_2;
+    xh3 =  p+q ;
+    xl3 =  p - xh3;
+    xl3 +=  q ;
+    xl3 +=  xl_1*xl_2+ xl1*xh2 + xh1*xl2;
+    // xl3 +=  xl_1*xl_2; // finish like this in the Müller's book
+    F2Sum(xh3, xl3, t, z);
+    return;
+    } 
 
 // inverse multiplication
 void Division2(double xh1, double xl1, double xh_inv, double xl_inv,double *xh3, double *xl3) {
   double xh_1, xl_1, xh_2, xl_2, t, z;
   double xh2,xl2;
-  if ( (xh_inv != 0) || (xl_inv != 0) ) {  //check order of operations seperately
+ /* if ( (xh_inv != 0) || (xl_inv != 0) ) {  //check order of operations seperately
   xh2= 1/xh_inv;
   xl2= 1/xl_inv; }
   else {  
   xh2=xh_inv; 
-  xl2=xl_inv; } 
-//  xh2= 1/xh_inv;
-//  xl2= xl_inv;
+  xl2=xl_inv; } */
+  xh2= 1/xh_inv;
+  xl2= xl_inv;
   Mult(xh1, xl1, xh2, xl2, xh3, xl3);
 /* VeltkampSplit(xh1, &xh_1, &xl_1); 
 VeltkampSplit(xh2, &xh_2, &xl_2); 
@@ -119,42 +140,6 @@ z /= xh2;
 F2Sum(t, z, xh3, xl3);
 return;
 } 
-
-   void Inverse_SquareRoot(double x1[3], double x2[3], double x3[3], double x4[3],double *xh3, double *xl3) {  //as pairs
-      double xh, xl, x_h, x_l, y0, y1, y2, y3, d0, d1, R3_0, R3_1;
-      double u0, u1, uu0, uu1, v0, v1, p0, p1, t0, t1 ,t2, t3, t4, t5, t6, t7;
-
-      Mult(x1[0],x2[0],x3[0],x4[0],&u0,&u1); // starting to scalar product 
-      Mult(x1[1],x2[1],x3[1],x4[1],&v0,&v1); 
-      Mult(x1[2],x2[2],x3[2],x4[2],&t0,&t1);  
-      Summation2(u0,u1,v0,v1,&t2,&t3);
-      Summation2(t0,t1,t2,t3,&t4,&t5);
-
-     y0 = sqrt(t4);                          //starting to the square root 
-     VeltkampSplit(y0,&xh, &xl) ;
-     Mult(xh, xl, xh, xl,&p0,&p1); 
-     y1 = t4 - p0;
-     y1 -= p1;
-     y1 += t5 ;
-     if(y0<0) { 
-     printf("error \n"); }
-     y1 = (y1 / y0)*0.5;         // y0 and y1 are pairs of the square root operation
-
-     Mult(y0, y1, y0, y1, &uu0,&uu1); 
-     Mult(y0, y1, uu0, uu1, &R3_0,&R3_1);    //pairs of R^3
-
-    // divide with 1's pairs with R^3 's pairs
-     y2=1;
-     *xh3 = y2/R3_0;
-     VeltkampSplit(*xh3, &xh, &xl) ;
-     VeltkampSplit(y2, &x_h, &x_l) ;
-     Mult(xh, xl, x_h, x_l,&t6,&t7); 
-     *xl3 = y2 - t6;
-     *xl3 -= t7;
-     *xl3 += y3;
-     *xl3 -= *xh3 * R3_1;
-     *xl3 /= R3_0;      
-     } 
 
    void unit_vector_longdouble(long double xh1, long double xh2, long double xh3, long double *X_unit, long double *Y_unit, long double *Z_unit) {
    long double alpha, F, dF, eps , scalar_new, max ,Z_new, Y_new, X_new ;
@@ -233,6 +218,24 @@ return;
       Summation2(t0,t1,t2,t3,xh3,xl3);
   }
 
+     void Inverse_SquareRoot(double R_h[3], double R_l[3], double *xh3, double *xl3) {  //as pairs
+      double xh, xl, x_h, x_l, y0, y1, y2, y3, d0, d1, R3_0, R3_1;
+      double u0, u1, uu0, uu1, v0, v1, p0, p1, t0, t1 ,t2, t3, t4, t5, t6, t7;
+
+      scalar_product(R_h, R_l, &t4, &t5);
+  
+     y0 = sqrt(t4);                          //starting to the square root 
+     VeltkampSplit(y0,&xh, &xl) ;
+     Mult(xh, xl, xh, xl, &p0, &p1); 
+     y1 = t4 - p0;
+     y1 -= p1;
+     y1 += t5 ;
+     if(y0<0) { 
+     printf("error \n"); }
+     y1 = (y1 / y0)*0.5;         // y0 and y1 are pairs of the square root operation
+     Mult(y0, y1, t4, t5, xh3, xl3); // k^2*sqrt(k)    
+     } 
+
    void unit_vector_pairwise(double xh[3], double xl[3], double *x_h, double *x_l) {
         double alpha, F, dF, eps , scalar_new, max ,Z_new, Y_new, X_new ;
         double xh11, xl11, xh22, xl22, xh33, xl33, yhh, yll, zh33, zl33, fhh, fll, dfhh, dfll;
@@ -294,17 +297,13 @@ int main(int argc, char **argv)
 int i;
 
 //double xh1=0.37797,xl1=0.000293,xh2=0.335593,xl2=0.0001233, xh3=0.3355674, I, J, K;
-double x1= -10.0, x2= 6.0, x3= 4.0 ;
-long double x1long= -10.0, x2long= 6.0, x3long= 4.0 ;
+double x1, x2;
 
 double xh_pair[]={-10.0, 6.0, 4.0}, xh_p[3];
 double xl_pair[]={0, 0, 0}, xl_p[3];
 
 double I, J, K;
 long double T, P, L;
-
-double x_pair1[] ={0.3,0.5,0.7};         // P1={ah,bh,ch}
-double x_pair2[] ={0.003,0.005,0.007};   // P2={al,bl,cl}
 
 double sum, sum_h, sum_l, sum_h2, sum_l2, prod, prod_h, prod_l, div_h, div_l; 
 double xh6, xl6, xh, xl, ch, cl, ch3, cl3, c, c3, c4, t_h, t_l ;
@@ -314,7 +313,7 @@ long int c_44, N;
 
 // SUMMATION ACCURACY TEST
 
-    FILE *kk,*kkkk;
+  /*  FILE *kk,*kkkk;
     kk=fopen("SUM2.txt","w");
     //usual double
     sum=0.0;
@@ -345,10 +344,12 @@ long int c_44, N;
     Summation2(sum_h, sum_l, ch, cl, &sum_h, &sum_l);
     Summation2(sum_h, sum_l, ch3, cl3, &sum_h2, &sum_l2);
     fprintf(kk,"%d %.20Le %.20Le %.20Le %.20Le\n",i, summ, -(sum-summ)/summ, -(sum_h-summ+sum_l)/summ, -(sum_h2-summ+sum_l2)/summ);  
-    } 
-   
+    } */
+
+
+    /*   // MULTIPLICATION ACCURACY TEST   
     FILE *kkk,*fff;
-    kkk=fopen("MULT4.txt","w");
+    kkk=fopen("MULT5.txt","w");
     //fff=fopen("MULT2.txt","w");
 
     prod=1.0;
@@ -374,20 +375,19 @@ long int c_44, N;
 
     // ORDINARY LONG DOUBLE MULTIPLICATION
     prod_long = prod_long*c_long;
-
-    // MULTIPLICATION ACCURACY TEST       
-    Mult(prod_h, prod_l, c3, 0, &prod_h, &prod_l);
-    Mult(t_h, t_l, ch, cl, &t_h, &t_l);
+    
+    Mult2(prod_h, prod_l, c3, 0, &prod_h, &prod_l);
+    Mult2(t_h, t_l, ch, cl, &t_h, &t_l);
     fprintf(kkk,"%d %.22Le %.22Le %.22Le  \n",i, prod_long, -(prod-prod_long)/prod_long, -(prod_h-prod_long+prod_l)/prod_long);
     //fprintf(fff,"%d %.22Le %.22Le %.22Le  \n",i, prod_long, -(prod-prod_long)/prod_long, -(t_h-prod_long+t_l)/prod_long);
-    } 
-    //}
-            printf("prod_h= %.16le prod_l=%.16le\n",prod_h,prod_l);
-            printf("th= %.16le tl=%.16le\n",t_h,t_l); 
+    } */
+    
+        /*    printf("prod_h= %.16le prod_l=%.16le\n",prod_h,prod_l);
+            printf("th= %.16le tl=%.16le\n",t_h,t_l); */
 
-  // DIVISION ACCURACY TEST  
+  // DIVISION ACCURACY TEST1 
 
-    FILE *tt;
+ /*   FILE *tt;
     tt=fopen("DIV.txt","w"); 
     
     N= 100000; //number of division operation
@@ -397,7 +397,7 @@ long int c_44, N;
     div_h = 1.0; //pairs
     div_l = 0;
 
-    for (i=0; i<=600; i++){
+    for (i=0; i<=20; i++){
           //ORDINARY DOUBLE DIVISION
     c4 =c4/3.0;
 
@@ -408,10 +408,47 @@ long int c_44, N;
     Division(div_h, div_l, 3.0, 0.0, &div_h, &div_l);
 
     fprintf(tt,"%d %.20Le %.20Le \n",i, -(c4-c_4)/c_4, -(div_h-c_4+div_l)/c_4 );  
-    } 
+    } */
 
 
-    // Unit vector with pair / double / longdouble ACCURACY TEST
+
+
+  // DIVISION ACCURACY TEST2
+
+    FILE *tt;
+    tt=fopen("DIV.txt","w"); 
+    long double A;
+    double B1, B2, B3;
+    
+    N= 100000; //number of division operation
+    c4  = 1.0;    //double
+    c_4 = 1.0;    //long double  
+    
+    div_h = 1.0; //pairs
+    div_l = 0;
+
+    for (i=0; i<=20; i++){
+          //ORDINARY DOUBLE DIVISION
+    c4 =c4/3.0;
+
+         //ORDINARY LONG-DOUBLE DIVISION
+    c_4 =c_4/3.0;
+
+         //SUMMATION WITH ARITHMETIC OPERATION
+    Division(div_h, div_l, 3.0, 0.0, &div_h, &div_l);     } 
+
+   // fprintf(tt,"%d %.20Le %.20Le \n",i, -(c4-c_4)/c_4, -(div_h-c_4+div_l)/c_4 ); 
+   printf("%.20Le %.20le %.20le %.20le \n", c_4, c4, div_h, div_l);  
+   A= pow(3,21);
+   B1= 1/c4;
+   B2= 1/div_h;
+   printf("%.20Le %.20le %.20le \n", A, B1, B2); 
+   printf(" A-double= %.20Le A-pairwise= %.20Le \n", A-B1, A-B2); 
+
+
+   
+
+  /*  // Unit vector with pair / double / longdouble ACCURACY TEST
    unit_vector(x1, x2, x3, &I, &J, &K);
    unit_vector_longdouble(x1long, x2long, x3long, &T, &P, &L);  
    unit_vector_pairwise(xh_pair, xl_pair, xh_p, xl_p);
@@ -424,11 +461,15 @@ long int c_44, N;
     printf("relative errors PAIRWISE / LONG-DOUBLE\n");
     printf("  for X component= %.16Le \n",-((xh_p[0]-T+xl_p[0])/T));
     printf("  for Y component= %.16Le \n",-((xh_p[1]-P+xl_p[1])/P));
-    printf("  for Z component= %.16Le \n",-((xh_p[2]-L+xl_p[2])/L)); 
-    
- fclose(kk);
- fclose(kkk); 
-fclose(fff); 
- fclose(tt); 
+    printf("  for Z component= %.16Le \n",-((xh_p[2]-L+xl_p[2])/L)); */
+
+
+  /* Inverse_SquareRoot(xh_pair, xl_pair, &x1, &x2);
+     printf("  x1= %.16le x2=%.16le \n",x1, x2); */
+
+// fclose(kk);
+// fclose(kkk); 
+// fclose(fff); 
+// fclose(tt); 
 return (0);  
 }
